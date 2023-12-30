b7a2da238557169caed61ab50edb11c99fdae0e8
Antonio <toninhoport@gmail.com>
2023-12-30 10:50:07 -0300

    fix cors

--- index.js
+++ index.js
@@ -10,9 +10,6 @@ const port = 3030;
 const cors = require("cors");
 const TASK_MODEL = require("./models/task");
 
-app.use(cors({ origin: "https://basel-1b2ea6jop-antonios-projects-92441c28.vercel.app", 
-credentials: true }));
-
 
 
 
